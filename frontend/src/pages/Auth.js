05bc14bd9dc8464730ffbef5dfe928428124bb2b
Krystal-G <lcs2020017@iiitl.ac.in>
2023-04-25 10:26:26 +0530

    Changes to auth.js

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -16,10 +16,11 @@ const Auth = (props) => {
         display: "flex",
         flex: "1 1 auto",
         width: "100%",
-        height: "100%",
+        height: "100vh",
+        // overflow: "hidden",
       }}
     >
-      <Grid container sx={{ flex: "1 1 auto" }}>
+      <Grid container sx={{ flex: "1" }}>
         <Grid
           xs={12}
           lg={6}
@@ -28,28 +29,9 @@ const Auth = (props) => {
             display: "flex",
             flexDirection: "column",
             position: "relative",
+            
           }}
         >
-          <Box
-            component="header"
-            sx={{
-              left: 0,
-              p: 3,
-              position: "fixed",
-              top: 0,
-              width: "100%",
-            }}
-          >
-            <Box
-              sx={{
-                display: "inline-flex",
-                height: 32,
-                width: 32,
-              }}
-            >
-              {/* <Logo /> */}
-            </Box>
-          </Box>
           {isLogin ? (
             <Login setIsLogin={setIsLogin} />
           ) : (
@@ -60,38 +42,23 @@ const Auth = (props) => {
           xs={12}
           lg={6}
           sx={{
-            alignItems: "center",
-            background:
-              "radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)",
-            color: "white",
-            display: "flex",
-            justifyContent: "center",
+            display: {xs:"none",md:"none",lg:"flex"},
+            flexDirection: "column",
+            textAlign:"center",
+            alignItems:"center",
+            justifyContent:"center",
+            borderRadius:"50px",
+            position: "relative",
             "& img": {
-              maxWidth: "100%",
+              maxWidth: {xs:"0",md:"100%"},
+              maxHeight:{xs:"0",md:"100%"},
+              height: "100%",
+              objectFit: "cover",
             },
           }}
-        >
-          <Box sx={{ p: 3 }}>
-            <Typography
-              align="center"
-              color="inherit"
-              sx={{
-                fontSize: "24px",
-                lineHeight: "32px",
-                mb: 1,
-              }}
-              variant="h1"
-            >
-              Welcome to{" "}
-              <Box component="a" sx={{ color: "#15B79E" }} target="_blank">
-                Devias Kit
-              </Box>
-            </Typography>
-            <Typography align="center" sx={{ mb: 3 }} variant="subtitle1">
-              A professional kit that comes with ready-to-use MUI components.
-            </Typography>
-            <img alt="" src="/assets/auth-illustration.svg" />
-          </Box>
+        >           
+          <img src={require("../components/auth/auth1.jpeg")}/>
+          
         </Grid>
       </Grid>
     </Box>
